1e0f323dbfdb1c6f829a3ed72b2cce5e32b9f2b8
pablo_cesar1995@hotmail.com <pablo_cesar1995@hotmail.com>
2017-10-15 16:21:18 -0500

    change

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-'use strict'
 const mongo = require('mongodb').MongoClient
 const client = require('socket.io').listen(4000).sockets
 const mongoose = require('mongoose')
@@ -55,59 +54,9 @@ mongoose.connect('mongodb://pablo95:passtodb@ds121015.mlab.com:21015/mychat', (e
 
         })
         //-----------------------------------------------------
-        app.listen(process.env.PORT || 3000, function () {
+        app.listen(process.env.PORT || 5000, function () {
             //Corriendo en el puerto...
-            console.log("Api Rest server listening in port " + port);
+            console.log("Api Rest server listening in port " + 5000);
         });
     }
 });
-
-/*
-//Connect to mongodb
-mongo.connect(url, (err, db) => {
-    if (err) {
-        throw err
-    }
-    console.log("Connected correctly to server");
-    
-    //Connect to socket.io
-    client.on('connection', (socket) => {
-        let chat = db.collection('chats')
-        //Function to send status
-        sendStatus = (s) => {
-            socket.emit('status', s)
-        }
-        //Get chats from mongo collection
-        chat.find().limit(100).sort({ _id: 1 }).toArray((err, res) => {
-            if (err) {
-                throw err
-            }
-            socket.emit('output', res)
-        })
-
-        socket.on('input', (data) => {
-            let name = data.name
-            let message = data.message
-
-            if (name == '' || message == '') {
-                sendStatus('Nombre y mensaje requeridos')
-            } else {
-                chat.insert({ name: name, message: message }, () => {
-                    client.emit('output', [data])
-                    sendStatus({
-                        message: 'Mensaje enviado',
-                        clear: true
-                    })
-                })
-            }
-        })
-
-        socket.on('clear', (data) => {
-            chat.remove({}, () => {
-                socket.emit('Borrados')
-            })
-        })
-
-    })
-
-});*/
